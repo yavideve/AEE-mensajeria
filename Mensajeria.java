06c79ceaf845ab8dc2a17aeaa04af0d55b9ac43f
yavideve <javijimmar07@gmail.com>
2026-05-24 20:06:16 +0200

    Update Mensajeria.java

--- Mensajeria.java
+++ Mensajeria.java
@@ -1,29 +1,58 @@
-import java.util.LinkedList;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  *
  * @author Jimenez Marquez Javier y Kilian David Corrales Pacheco
  */
-public class Cola {
-    private LinkedList<Object> tCola = new LinkedList<>();
+public class Mensajeria {
 
-    public Cola() {
-    }
-
-    public void meterEnCola(Object obj) {
-        tCola.offer(obj);
-    }
-
-    public Object sacarDeCola() {
-        
-            if (tCola.isEmpty()) {
-                System.out.println("La cola esta vacia");
+    public static void main(String[] args) {
+        // Entorno
+        short op;
+        String ipv4, text, user;
+        Cola colaMensajes = new Cola();
+        Mensaje mensaje;
+        // Algoritmo
+        System.out.println("1. Envío instantaneo");
+        System.out.println("2. Enviar mensaje");
+        System.out.println("3. Recibir mensaje");
+        System.out.println("4. Salir");
+        System.out.println("Elija opción");
+        op = Leer.datoShort();
+        while (op != 4) {
+            switch (op) {
+                case 1:
+                    mensaje = new Mensaje();
+                    colaMensajes.meterEnCola(mensaje);
+                    System.out.println(mensaje.getUsuario() + " " + mensaje.getIp() + " " + mensaje.getText());
+                    break;
+                case 2:
+                    System.out.println("Introduce ip");
+                    ipv4 = Leer.dato();
+                    System.out.println("Introduce texto");
+                    text = Leer.dato();
+                    System.out.println("Introduce Usuario");
+                    user = Leer.dato();
+                    mensaje = new Mensaje(user, ipv4, text);
+                    colaMensajes.meterEnCola(mensaje);
+                    System.out.println(mensaje.getUsuario() + " " + mensaje.getIp() + " " + mensaje.getText());
+                    break;
+                case 3:
+                    mensaje = (Mensaje) colaMensajes.sacarDeCola();
+                    System.out.println("Usuario: " + mensaje.getUsuario());
+                    System.out.print(mensaje.fecha.get(Calendar.DATE)); 
+                    System.out.print("/" + (mensaje.fecha.get(Calendar.MONTH) + 1)); 
+                    System.out.println("/" + mensaje.fecha.get(Calendar.YEAR)); 
+                    System.out.println("IP: " + mensaje.getIp());
+                    System.out.println("Texto: ");
+                    System.out.println(mensaje.getText());
+                    break;
             }
-            return tCola.poll();
+            System.out.println("Elija opción");
+            op = Leer.datoShort();
+        }
 
-    }
-
-    public boolean esColaVacia() {
-        return tCola.isEmpty();
     }
 }
