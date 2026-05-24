6024cc0df64c45a0f853eec96dcf7b1e4b561616
kiliandavidcorrales25-collab <kiliandavidcorrales.25@campuscamara.es>
2026-05-24 21:39:58 +0200

    Update Mensajeria.java

--- Mensajeria.java
+++ Mensajeria.java
@@ -1,107 +1,80 @@
+
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 /**
  *
  * @author Jimenez Marquez Javier y Kilian David Corrales Pacheco
  */
-public class Mensaje {
-    private final String APIPA = "169.254.0.1";
-    private String usuario = "System";
-    GregorianCalendar fecha = new GregorianCalendar();
-    private String ip = APIPA;
-    private String texto = "POR DEFECTO";
-    
-    public Mensaje() {}
-    public Mensaje(String usuario, GregorianCalendar fecha, String ip, String texto) {
-        this.usuario = usuario;
-        this.fecha = fecha;
-        this.ip = ip;
-        this.texto = texto.toUpperCase();
-    }
-    public Mensaje(String usuario, String ip, String texto) {
-        
-    }
-    public String getAPIPA() {
-        return this.APIPA;
-    }
+public class Mensajeria {
 
-    public String getUsuario() {
-        return this.usuario;
-    }
+    public static void main(String[] args) {
+        // Entorno
+        short op;
+        char respuesta;
+        boolean continuar = false;
+        String ipv4, text, user;
+        Cola colaMensajes = new Cola();
+        Mensaje mensaje;
+        // Algoritmo
+        System.out.println("1. Envío instantaneo");
+        System.out.println("2. Enviar mensaje");
+        System.out.println("3. Recibir mensaje");
+        System.out.println("4. Salir");
+        System.out.println("Elija opción");
+        op = Leer.datoShort();
+        while (op != 0) {
+            switch (op) {
+                case 1:
+                    mensaje = new Mensaje();
+                    colaMensajes.meterEnCola(mensaje);
+                    System.out.println(mensaje.getUsuario() + " " + mensaje.getIp() + " " + mensaje.getText());
+                    break;
+                case 2:
+                    System.out.println("Introduce Usuario");
+                    user = Leer.dato();
+                    System.out.println("Introduce ip");
+                    ipv4 = Leer.dato();
+                    System.out.println("Introduce texto");
+                    text = Leer.dato();
+                    mensaje = new Mensaje(user, ipv4, text);
+                    colaMensajes.meterEnCola(mensaje);
+                    System.out.println(mensaje.getUsuario() + " " + mensaje.getIp() + " " + mensaje.getText());
+                    break;
+                case 3:
+                    if (colaMensajes.esColaVacia() == false) {
+                        mensaje = (Mensaje) colaMensajes.sacarDeCola();
+                        System.out.println("Usuario: " + mensaje.getUsuario());
+                        System.out.print("Fecha: " + mensaje.fecha.get(Calendar.DATE));
+                        System.out.print("/" + (mensaje.fecha.get(Calendar.MONTH) + 1));
+                        System.out.println("/" + mensaje.fecha.get(Calendar.YEAR));
+                        System.out.println("IP: " + mensaje.getIp());
+                        System.out.println("Texto: ");
+                        System.out.println(mensaje.getText());
+                    } else {
+                        System.out.println("No hay más mensajes");
 
-    public GregorianCalendar getFecha() {
-        return this.fecha = new GregorianCalendar();
-    }
+                    }
+                    break;
+                case 4:
+                    if (colaMensajes.esColaVacia() == true ) {
+                           op = 0;
+                     } else if  (colaMensajes.esColaVacia() == false) {
+                        System.out.println("Hay mensajes pendientes de recibir ¿Salir de todas formas? (S/N)");
+                        respuesta = Leer.datoChar();
+                        if (respuesta == 'S') {
+                            op = 0;
+                        } else if (respuesta != 'S'){
+                            break;
+                        }
 
-    public String getIp() {
-       return this.ip;
-    }
-
-    public String getText() {
-        return this.texto;
-    }
-
-    public boolean esValido(String ip) {
-        String ipv4 = "\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}";
-        if (ipv4.equals(ip)) {
-            return true;
-        } else {
-            return false;
+                    }
+                 
+                    }
+               if (op != 0 ) {
+                        System.out.println("Elija opción");
+                        op = Leer.datoShort();
+            }
         }
-    }
-
-    public String encripta(String msg) {
-        int i;
-        String invertido = "";
-        char ultimocaracter;
-        String desplazado = "";
-        String encriptado = "";
-
-        for (i = msg.length() - 1; i >= 0; i--) {
-            invertido += msg.charAt(i);
-        }
-        ultimocaracter = invertido.charAt(invertido.length() - 1);
-        for (i = invertido.length() - 1; i >= 1; i--) {
-            desplazado = invertido.charAt(i) + desplazado;
-        }
-        desplazado = ultimocaracter + desplazado;
-
-        for (i = desplazado.length() - 1; i >= 0; i--) {
-            encriptado += desplazado.charAt(i) + 3;
-        }
-        return encriptado;
-    }
-
-    public String desencripta(String msg) {
-        int i;
-        String resultado = "";
-        String invertido = "";
-        String desplazado = "";
-        char primercaracter;
-        String desencriptado = "";
-
-        for (i = msg.length() - 1; i >= 0; i--) {
-            desencriptado += msg.charAt(i) - 3;
-        }
-        primercaracter = desencriptado.charAt(0);
-        for (i = 0; i <= desencriptado.length() - 2; i++) {
-            desplazado = desplazado + desencriptado.charAt(i);
-        }
-        desplazado = desplazado + primercaracter;
-
-        for (i = desplazado.length() - 1; i >= 0; i--) {
-            resultado += desplazado.charAt(i);
-        }
-        return resultado;
-    }
-
-    public String reverse(String cad) {
-        int i;
-        String inverso = "";
-        for (i = cad.length() - 1; i >= 0; i--) {
-            inverso += cad.charAt(i);
-        }
-        return inverso;
     }
 }
-
