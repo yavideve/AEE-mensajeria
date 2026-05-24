9fb3458424b9242719307bef8775d6ec10ebec42
yavideve <javijimmar07@gmail.com>
2026-05-24 21:56:29 +0200

    Update cola.java

--- cola.java
+++ cola.java
@@ -5,23 +5,25 @@ import java.util.LinkedList;
  * @author Jimenez Marquez Javier y Kilian David Corrales Pacheco
  */
 public class Cola {
+    // Atributos
     private LinkedList<Object> tCola = new LinkedList<>();
-
+    
+    // Métodos
     public Cola() {
     }
 
     public void meterEnCola(Object obj) {
         tCola.offer(obj);
-    }
-
+    } // Fin función
+    
     public Object sacarDeCola() {
         
             if (tCola.isEmpty() == false) {
                return tCola.poll();
             }
         return null;
-    }
+    } // Fin función
     public boolean esColaVacia() {
         return tCola.isEmpty();
-    }
+    } // Fin función
 }
